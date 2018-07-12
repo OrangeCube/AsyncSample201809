bb7ccb08845040a4591a69be236d89e8b921dbc6
ppcuni <ppc_kh@yahoo.co.jp>
2018-07-12 20:17:18 +0900

    テキスト送りサンプル

---
+++ AsyncSample201809/Assets/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static partial class TaskExtensions
+{
+    /// <summary>
+    /// タスクを呼び出しの終端処理。タスク実行中の未ハンドルな例外があったらログに出す
+    /// </summary>
+    /// <param name="task"></param>
+    public static async void FireAndForget(this Task task)
+    {
+        if (task == null) return;
+
+        Exception ex = null;
+        await task.ContinueWith(t => ex = t.Exception);
+
+        if (ex != null)
+            Debug.LogException(ex);
+        else if (!task.IsCanceled)
+            Debug.Log($"{task}完了");
+        else
+            Debug.Log($"{task}キャンセル");
+    }
+}
---
+++ AsyncSample201809/Assets/テキストページ送り.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class テキストページ送り : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text;
+
+    [SerializeField]
+    private Button _button;
+
+    void Start()
+    {
+        テキストページ送りAsync().FireAndForget();
+    }
+
+    private async Task テキストページ送りAsync()
+    {
+        for (var i = 1; i < 10; i++)
+        {
+            _text.text = $"{i}ページ目を表示。画面タップを待つ";
+            await _button.OnClickAsObservable().First().ToTask();
+        }
+        _text.text = "おわり";
+    }
+}
