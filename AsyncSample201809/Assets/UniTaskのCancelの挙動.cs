fc7da49bdb2779fa1491495325a45732187479fa
ppcuni <ppc_kh@yahoo.co.jp>
2018-08-10 17:50:24 +0900

    UniTaskのキャンセルの挙動について

---
+++ AsyncSample201809/Assets/UniTaskのCancelの挙動.cs
@@ -0,0 +1,38 @@
+﻿using UniRx;
+using UniRx.Async;
+using UnityEngine;
+
+public class UniTaskのCancelの挙動 : MonoBehaviour
+{
+    void Start()
+    {
+        RunAsync().FireAndForget();
+    }
+
+    private async UniTask RunAsync()
+    {
+        var a = RunAsyncInternal();
+        try
+        {
+            await a;
+        }
+        finally
+        {
+            Debug.Log("RunAsync status= " + a.Status);
+        }
+    }
+
+    private async UniTask RunAsyncInternal()
+    {
+        var tcs = new UniTaskCompletionSource();
+        tcs.TrySetCanceled();
+        try
+        {
+            await tcs.Task;
+        }
+        finally
+        {
+            Debug.Log("RunAsyncInternal status= " + tcs.Task.Status);
+        }
+    }
+}
