a48d1f2e56f2bf1ab4fabfec33f31eb246bcc8c5
ppc <ppc_kh@yahoo.co.jp>
2018-09-12 17:52:17 +0900

    ライブコーディング用スケルトンコード

---
+++ AsyncSample201809/Assets/ライブコーディング用.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UniRx;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ライブコーディング用 : TypedMonoBehaviour
+{
+    [SerializeField]
+    private Text _text;
+
+    [SerializeField]
+    private RawImage _image;
+
+    [SerializeField]
+    private Button _button;
+
+    [SerializeField]
+    private RectTransform _選択肢Container;
+
+    [SerializeField]
+    private GameObject _選択肢Prefab;
+
+    [SerializeField]
+    private LoadingPanel _loadinPanel;
+
+    [SerializeField]
+    private Button _skipButton;
+
+    void Start()
+    {
+        RunAsync().FireAndForget();
+    }
+
+    private async UniTask RunAsync()
+    {
+    }
+}
